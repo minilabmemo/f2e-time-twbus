81cd499ce8e4e515e482372860930da4f9908846
minilabmemo <minilabmemo@gmail.com>
2023-10-13 15:48:03 +0800

    Add: main city button

--- src/pages/CityBusHome.tsx
+++ src/pages/CityBusHome.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import main from '../images/home_main.svg';
-
+import carbon_bus from '../images/carbon_bus.svg';
 const CityBusHome = () => {
 
 
   return (
     <>
       <section className="main">
-        <img src={main} alt="main_photo" width={'100%'} />
+        <img className="city-icon" src={main} alt="main_photo" width={'100%'} />
         <div className="main-text">
           <div >taiwan bus+</div>
           <div >台 灣 公 車 動 態</div>
@@ -15,10 +15,39 @@ const CityBusHome = () => {
         </div>
       </section>
 
+      <section className="cities">
+        <div className="city-card city-color-1">
+          <img className="city-icon" src={carbon_bus} alt="carbon_bus" />
+          <div className="city-name">台北市 / 新北市</div>
+          <div className="city-name-en"> Taipei / New Taipei</div>
+        </div>
 
-
-
-      <section></section>
+        <div className="city-card city-color-2">
+          <img className="city-icon" src={carbon_bus} alt="carbon_bus" />
+          <div className="city-name">桃園市</div>
+          <div className="city-name-en">Taoyuan</div>
+        </div>
+        <div className="city-card city-color-3">
+          <img className="city-icon" src={carbon_bus} alt="carbon_bus" />
+          <div className="city-name">台中市</div>
+          <div className="city-name-en">Taichung</div>
+        </div>
+        <div className="city-card city-color-4">
+          <img className="city-icon" src={carbon_bus} alt="carbon_bus" />
+          <div className="city-name">台南市</div>
+          <div className="city-name-en">Tainan</div>
+        </div>
+        <div className="city-card city-color-5">
+          <img className="city-icon" src={carbon_bus} alt="carbon_bus" />
+          <div className="city-name">高雄市</div>
+          <div className="city-name-en"> 高雄市</div>
+        </div>
+        <div className="city-card city-color-6">
+          <img className="city-icon" src={carbon_bus} alt="carbon_bus" />
+          <div className="city-name">其他地區</div>
+          <div className="city-name-en">Other City</div>
+        </div>
+      </section>
     </>
   );
 };
