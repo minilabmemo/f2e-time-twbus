004d775be0f540b478a4407b55d332adc77a75fa
minilabmemo <minilabmemo@gmail.com>
2023-10-19 11:22:17 +0800

    Fix: tidy

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,7 @@ import { Header } from './components/layout/header';
 import PetFinder from './pages/CityBusHome';
 import { Footer } from './components/layout/Footer';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-
-import { About } from './components/News';
-import Stores from './components/Stores';
-import PetDetailsPage from './components/PetDetailsPage';
 import { URI_PET_FIND_PREFIX, URI_SEARCH, URI_STOPS } from './utils/const';
-import PetChart from './components/PetChart';
 import { Wait } from './components/Wait';
 import { BusRouteSearch } from './pages/BusRouteSearch';
 import { BusRouteStops } from './pages/BusRouteStops';
@@ -32,13 +27,9 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<BaseLayout><PetFinder /></BaseLayout>} />
-        <Route path="/about" element={<BaseLayout><About /></BaseLayout>} />
         <Route path={`/${URI_PET_FIND_PREFIX}`} element={<BaseLayout><Wait /></BaseLayout>} />
         <Route path={`${URI_SEARCH}`} element={<BaseLayout><BusRouteSearch /></BaseLayout>} />
         <Route path={`${URI_STOPS}`} element={<BaseLayout><BusRouteStops /></BaseLayout>} />
-        <Route path="/chart" element={<BaseLayout><PetChart /></BaseLayout>} />
-        <Route path="/stores" element={<BaseLayout><Stores /></BaseLayout>} />
-        <Route path={`/${URI_PET_FIND_PREFIX}/:id`} element={<BaseLayout><PetDetailsPage /></BaseLayout>} />
       </Routes>
     </HashRouter>
   );
